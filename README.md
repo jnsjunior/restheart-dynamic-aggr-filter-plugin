c972dc67a71b54468b9951d8d96e8e0d5ffcf8cd
João Norberto de Souza Junior <joao.norberto@spro.com.br>
2022-08-09 17:33:24 -0300

    Version 1.0.0

---
+++ README.md
@@ -0,0 +1,81 @@
+# RestHeart Dynamic Aggregation Filter Plugin
+
+[![MIT License][license-shield]][license-url]
+
+The purpose of this plugin is to provide a service to run [RestHeart](https://restheart.org/) aggregation with dynamic variable fields (even using [MongoDB](https://www.mongodb.com/) operators).
+This aproach avoid turning off [aggregation-check-operators](https://restheart.org/docs/mongodb-rest/aggregations#Security%20considerations:~:text=Security%20considerations). This behavior protect data from undesirable malicious query injection.
+
+## Getting Started
+
+### Installation
+
+1. Clone the repo inside RestHeart's plugin folder
+   ```sh
+   cd /opt/restheart/plugins
+   git clone https://github.com/jnsjunior/restheart-dynamic-aggr-filter-plugin.git
+   ```
+2. Restart RestHeart instance (check [RestHeart](https://restheart.org/docs/setup) documentation)
+
+## Usage example
+
+1. Create a new database
+   ```sh
+   curl --request PUT \
+        --url https://<your-restheart-host>/mydb \
+        --header 'authorization: Basic <your-restheart-credentials>'
+   ```
+2. Create a new collection
+   ```sh
+   curl --request PUT \
+        --url https://<your-restheart-host>/mydb/mycoll \
+        --header 'authorization: Basic <your-restheart-credentials>'
+   ```
+3. Add new documents to the new collection
+   ```sh
+   curl --request POST \
+        --url https://<your-restheart-host>/mydb/mycoll \
+        --header 'authorization: Basic <your-restheart-credentials>'
+        --data '[{ "item": "journal", "qty": 25, "size": { "h": 14, "w": 21, "uom": "cm" }, "status": "A" },{ "item": "notebook", "qty": 50, "size": { "h": 8.5, "w": 11, "uom": "in" }, "status": "A" },{ "item": "paper", "qty": 100, "size": { "h": 8.5, "w": 11, "uom": "in" }, "status": "D" },{ "item": "planner", "qty": 75, "size": { "h": 22.85, "w": 30, "uom": "cm" }, "status": "D" },{ "item": "postcard", "qty": 45, "size": { "h": 10, "w": 15.25, "uom": "cm" }, "status": "A" }]'
+   ```
+4. Create a new aggregation to the new collection
+   ```sh
+   curl --request PUT \
+        --url https://<your-restheart-host>/mydb/mycoll \
+        --header 'authorization: Basic <your-restheart-credentials>'
+        --data '{"aggrs": [{"stages": [{ "$match": { "qty": { "$var": "n" } } },{ "$project": { "_id": "$item", "qty": "$qty" } }],"type": "pipeline","uri": "myagg"}]}'
+   ```
+5. Call plugin
+   ```sh
+   curl --request POST \
+        --url https://<your-restheart-host>/_dynamic_aggr_filter \
+        --header 'authorization: Basic <your-restheart-credentials>'
+        --data '{"Database": "mydb","Collection": "mycoll","Aggregation": "myagg","Variables": [{"n": 25}]}'
+   ```
+   Result:
+   ```sh
+   [{"_id":"journal","qty":25}]
+   ```
+6. Another example using MongoDB operators
+   ```sh
+   curl --request POST \
+        --url https://<your-restheart-host>/_dynamic_aggr_filter \
+        --header 'authorization: Basic <your-restheart-credentials>'
+        --data '{"Database": "mydb","Collection": "mycoll","Aggregation": "myagg","Variables": [{"n": {"$gte": 25, "$lt": 100}}]}'
+   ```
+   Result:
+   ```sh
+   [{"_id":"journal","qty":25},{"_id":"notebook","qty":50},{"_id":"planner","qty":75},{"_id":"postcard","qty":45}]
+   ```
+
+## License
+
+Distributed under the MIT License. See `LICENSE` for more information.
+
+## Contact
+
+João N de Souza Jr - [@jnsjunior](https://twitter.com/jnsjunior) - jnsjunior@gmail.com
+
+Project Link: [https://github.com/jnsjunior/restheart-dynamic-aggr-filter-plugin](https://github.com/jnsjunior/restheart-dynamic-aggr-filter-plugin)
+
+[license-shield]: https://img.shields.io/github/license/othneildrew/Best-README-Template.svg?style=for-the-badge
+[license-url]: https://github.com/othneildrew/Best-README-Template/blob/master/LICENSE.txt
---
+++ index.js
@@ -0,0 +1,160 @@
+const BsonUtils = Java.type("org.restheart.utils.BsonUtils");
+const BsonArray = Java.type("org.bson.BsonArray");
+const BsonDocument = Java.type("org.bson.BsonDocument");
+
+export const options = {
+    name: "restheart-dynamic-aggr-filter-plugin",
+    description: "RestHeart Dynamic Aggregation Filter Plugin",
+    uri: "/_dynamic_aggr_filter",
+    secured: true
+};
+
+function replaceObjects(obj, targetProp, targetValue, replaceValue) {
+    function getObject(theObj) {
+        if (theObj instanceof Array) {
+            for (let i = 0; i < theObj.length; i++) {
+                getObject(theObj[i]);
+            }
+        }
+        else {
+            for (let prop in theObj) {
+                if (theObj.hasOwnProperty(prop)) {
+                    if (prop === targetProp) {
+                        if (theObject[prop] === targetValue) {
+                            theObject[prop] = replaceValue;
+                        }
+                    }
+                    if (theObj[prop] instanceof Object ||
+                        theObj[prop] instanceof Array) {
+                        if (theObj[prop] instanceof Object) {
+                            if (theObj[prop].hasOwnProperty(targetProp)) {
+                                if (theObj[prop][targetProp] === targetValue) {
+                                    theObj[prop] = replaceValue;
+                                }
+                            }
+                        }
+                        getObject(theObj[prop]);
+                    }
+                }
+            }
+        }
+    }
+    getObject(obj);
+}
+
+function replaceVars(pipeline, vars) {
+    for (var v in vars) {
+        for (var prop in vars[v]) {
+            replaceObjects(pipeline, '$var', prop, vars[v][prop]);
+        }
+    }
+    return pipeline;
+}
+
+function getPipeline(db, coll, pipelineName) {
+
+    const prop = db.getCollection("_properties", BsonDocument.class);
+
+    var agg = JSON.stringify([
+        { "$unwind": "$aggrs" },
+        {
+            "$match":
+            {
+                "_id": `_properties.${coll}`,
+                "aggrs.uri": pipelineName
+            }
+        },
+        {
+            "$project": {
+                "_id": 0,
+                "aggrs.stages": 1
+            }
+        }
+    ]);
+
+    var res = prop.aggregate(BsonUtils.parse(agg)).iterator().next();
+    var pipeline = res.aggrs.stages;
+    var pipelineStr = BsonUtils.toJson(pipeline).replaceAll("_$", "$$");
+
+    return JSON.parse(pipelineStr);
+}
+
+function runPipeline(coll, pipeline) {
+
+    var agg = JSON.stringify(pipeline);
+
+    let it = coll.aggregate(BsonUtils.parse(agg)).iterator();
+
+    let results = new BsonArray();
+
+    while (it.hasNext()) {
+        results.add(it.next());
+    }
+
+    return BsonUtils.toJson(results);
+
+}
+
+function raiseError(msg, response) {
+    response.setInError(400, msg);
+    LOGGER.error(msg);
+}
+
+export function handle(request, response) {
+    LOGGER.info("RestHeart Dynamic Aggregation Filter Plugin CALL");
+
+    var content = JSON.parse(request.getContent());
+
+    if (!(content instanceof Object)) {
+        raiseError('Request body must be an object', response);
+        return;
+    }
+
+    var dbname = content["Database"];
+    var collname = content["Collection"];
+    var agg = content["Aggregation"];
+    var vars = content["Variables"];
+
+    if(typeof dbname === 'undefined') {
+        raiseError('Missing Database body parameter', response);
+        return;
+    }
+
+    if(typeof collname === 'undefined') {
+        raiseError('Missing Collection body parameter', response);
+        return;
+    }
+
+    if(typeof agg === 'undefined') {
+        raiseError('Missing Aggregation body parameter', response);
+        return;
+    }
+
+    if(typeof vars === 'undefined') {
+        raiseError('Missing Variables body parameter', response);
+        return;
+    }
+
+    LOGGER.info(`[${dbname}][${collname}][${agg}] CALL`);
+
+    LOGGER.debug(`[Database] ${dbname}`);
+    LOGGER.debug(`[Collection] ${collname}`);
+    LOGGER.debug(`[Aggregation] ${agg}`);
+    LOGGER.debug(`[Variables] ${JSON.stringify(vars)}`);
+
+    const db = mclient.getDatabase(dbname);
+
+    var raw_pipeline = getPipeline(db, collname, agg);
+    var pipeline = replaceVars(raw_pipeline, vars);
+
+    LOGGER.debug(JSON.stringify(pipeline));
+
+    const coll = db.getCollection(collname, BsonDocument.class);
+    var results = runPipeline(coll, pipeline);
+    
+    LOGGER.info(`[${dbname}][${collname}][${agg}] RESULT: ${results.length} documents`);
+    LOGGER.debug(JSON.stringify(results));
+
+    response.setContent(results);
+    response.setContentTypeAsJson();
+}
